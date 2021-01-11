e6ecf2236a780d438b8009dd9b3a72c1b223fee0
faizinur <inurfaizi@gmail.com>
2021-01-11 10:53:33 +0700

    NAMBAHinPLUGIN DARI EGI

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import uuid from './Uuid';
+import * as log from './Consoles';
 import Camera from './Camera';
 import Connection from './Connection';
 import Device from './Device';
@@ -5,6 +7,8 @@ import Fingerprint from './Fingerprint';
 import Geolocation from './Geolocation';
 
 export {
+    uuid,
+    log,
     Camera,
     Connection,
     Device,
