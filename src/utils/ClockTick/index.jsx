0174397eb7029c4d5049336163f4abcecd0142fb
faizinur <inurfaizi@gmail.com>
2021-01-20 21:54:15 +0700

    perbaiki jam di system info

--- src/utils/ClockTick/index.jsx
+++ src/utils/ClockTick/index.jsx
@@ -4,20 +4,26 @@ const ClockTick = (clock) => {
     h = parseInt(h);
     m = parseInt(m);
     s = parseInt(s);
-    if (s < 59) {
-        s = ++s;
-    } else {
-        s = 0;
-        if (m < 59) {
-            m = ++m;
-        } else {
-            m = 0;
-            if (h < 24) {
-                h = ++h;
-            }
-        }
-    }
+
+    s = (s < 59) ? ++s : 0;
+    m = (m < 59) ? ((s == 0) ? ++m : m) : 0;
+    h = (h < 23) ? ((m == 0) ? ++h : h) : 0;
+    
     return `${day} ${h < 10 ? '0' + h : h}:${m < 10 ? '0' + m : m}:${s < 10 ? '0' + s : s}`;
 }
 
 export default ClockTick;
+
+// if (s < 59) {
+//     s = ++s;
+// } else {
+//     s = 0;
+//     if (m < 59) {
+//         m = ++m;
+//     } else {
+//         m = 0;
+//         if (h < 24) {
+//             h = ++h;
+//         }
+//     }
+// }
